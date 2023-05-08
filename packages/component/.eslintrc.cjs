fee7c008cd43522e883e8f7ec4955d6225838718
easymean <easymean@ncsoft.com>
2023-05-08 18:49:13 +0900

    공통 eslint 설정

--- packages/component/.eslintrc.cjs
+++ packages/component/.eslintrc.cjs
@@ -1,14 +1,12 @@
+const baseConfig = require('../../.eslintrc');
+
 module.exports = {
-  env: { browser: true, es2020: true },
-  extends: [
-    'eslint:recommended',
-    'plugin:@typescript-eslint/recommended',
-    'plugin:react-hooks/recommended',
-  ],
-  parser: '@typescript-eslint/parser',
-  parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
-  plugins: ['react-refresh'],
-  rules: {
-    'react-refresh/only-export-components': 'warn',
-  },
-}
+	...baseConfig,
+	extends: ['plugin:@typescript-eslint/recommended', 'plugin:react-hooks/recommended'],
+	parser: '@typescript-eslint/parser',
+	parserOptions: { sourceType: 'module' },
+	plugins: ['react-refresh'],
+	rules: {
+		'react-refresh/only-export-components': 'warn',
+	},
+};
