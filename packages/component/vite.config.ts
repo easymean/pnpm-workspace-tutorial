389eed93a3a770b600cd4b354833f4e517086504
easymean <easymean@ncsoft.com>
2023-05-08 17:51:39 +0900

    vite 빌드 설정 변경

--- packages/component/vite.config.ts
+++ packages/component/vite.config.ts
@@ -1,7 +1,22 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import { resolve } from 'path';
+import dts from 'vite-plugin-dts';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+	build: {
+		lib: {
+			entry: resolve(__dirname, 'src/index.ts'),
+			formats: ['es'],
+		},
+		rollupOptions: {
+			output: {
+				format: 'es',
+				entryFileNames: 'index.js',
+				dir: 'dist/',
+			},
+		},
+	},
+	plugins: [react(), dts()],
+});
