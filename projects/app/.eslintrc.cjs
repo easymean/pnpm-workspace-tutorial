801f02a61c1a9a271177028f69f4b8ce37ff75bd
easymean <easymean@ncsoft.com>
2023-05-09 13:32:16 +0900

    공통 eslint 상속

--- projects/app/.eslintrc.cjs
+++ projects/app/.eslintrc.cjs
@@ -1,12 +1,10 @@
+const baseConfig = require('../../.eslintrc');
+
 module.exports = {
-	env: { browser: true, es2020: true },
-	extends: [
-		'eslint:recommended',
-		'plugin:@typescript-eslint/recommended',
-		'plugin:react-hooks/recommended',
-	],
+	...baseConfig,
+	extends: ['plugin:@typescript-eslint/recommended', 'plugin:react-hooks/recommended'],
 	parser: '@typescript-eslint/parser',
-	parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
+	parserOptions: { sourceType: 'module' },
 	plugins: ['react-refresh'],
 	rules: {
 		'react-refresh/only-export-components': 'warn',
